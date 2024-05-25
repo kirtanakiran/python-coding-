bd2b23afaebe2b822ace8f5e1abe40e70597545c
KIRTANA KIRAN <122041511+kirtanakiran@users.noreply.github.com>
2024-05-25 12:50:48 +0530

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -1,3 +1,9 @@
+#Go through the array, one value at a time.
+#For each value, compare the value with the next value.
+#If the value is higher than the next one, swap the values so that the highest value comes last.
+#Go through the array as many times as there are values in the array.
+#time complexity - O(n^2)
+
 def bubble_sort(arr):
     n = len(arr)
     print(n)
