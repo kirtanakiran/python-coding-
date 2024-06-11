a299e27047c10ba1f676a1504c3b13d0b02fe2a9
KIRTANA KIRAN <122041511+kirtanakiran@users.noreply.github.com>
2024-06-11 17:38:32 +0530

    Update integer to roman.py

--- 150 leet/integer to roman.py
+++ 150 leet/integer to roman.py
@@ -2,7 +2,7 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         Roman = ""
         storeIntRoman = [[1000, "M"], [900, "CM"], [500,"D"], [400,"CD"], [100,"C"], [90, "XC"], [50, "L"], [40, "XL"], [9, "IX"], [5,"V"], [4, "IV"], [1, "I"]]
-        for i ina range(len(storwIntRoman)):
+        for i in range(len(storwIntRoman)):
             while num >= storeIntRoman[i][0]:
                 Roman += storeIntRoman[i][1]
                 num -= storeIntRoman[i][0]
